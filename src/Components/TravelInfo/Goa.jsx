3a2513c98692da2dba3745349df751a86d9d579a
Siddhantm09 <mahatosiddhant29@gmail.com>
2023-02-02 22:32:15 +0530

    deploy error

--- src/Components/TravelInfo/Goa.jsx
+++ src/Components/TravelInfo/Goa.jsx
@@ -1,36 +1,7 @@
 import React from "react";
-// import "react-responsive-carousel/lib/styles/carousel.min.css";
-// import { Carousel } from "react-responsive-carousel";
-//import img1 from "../../Assets/SliderImages/goaSlider/img1.jpg";
-// import img2 from "../../Assets/SliderImages/goaSlider/img2.jpg";
-// import img3 from "../../Assets/SliderImages/goaSlider/img3.jpg";
-// import img4 from "../../Assets/SliderImages/goaSlider/img4.jpg";
-// import img5 from "../../Assets/SliderImages/goaSlider/img5.jpg";
-// import img6 from "../../Assets/SliderImages/goaSlider/img6.jpeg";
-// import img7 from "../../Assets/SliderImages/goaSlider/img7.jpeg";
 
 const Goa = () => {
-  return (
-    <div>
-      {/* <Carousel infiniteLoop autoPlay>
-        <div className="image">
-          <img src={img2} alt="" />
-        </div>
-        <div className="image">
-          <img src={img2} alt="" />
-        </div>
-        <div className="image">
-          <img src={img3} alt="" />
-        </div>
-        <div className="image">
-          <img src={img4} alt="" />
-        </div>
-        <div className="image">
-          <img src={img5} alt="" />
-        </div>
-      </Carousel> */}
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Goa;
