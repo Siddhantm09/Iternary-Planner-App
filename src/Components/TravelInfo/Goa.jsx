a1fb41fd54c66eddb17c6c9b35919a85e59eac75
Siddhantm09 <mahatosiddhant29@gmail.com>
2023-02-02 22:30:30 +0530

    .

--- src/Components/TravelInfo/Goa.jsx
+++ src/Components/TravelInfo/Goa.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { Carousel } from "react-responsive-carousel";
+// import "react-responsive-carousel/lib/styles/carousel.min.css";
+// import { Carousel } from "react-responsive-carousel";
 //import img1 from "../../Assets/SliderImages/goaSlider/img1.jpg";
 // import img2 from "../../Assets/SliderImages/goaSlider/img2.jpg";
 // import img3 from "../../Assets/SliderImages/goaSlider/img3.jpg";
@@ -8,8 +8,6 @@ import { Carousel } from "react-responsive-carousel";
 // import img5 from "../../Assets/SliderImages/goaSlider/img5.jpg";
 // import img6 from "../../Assets/SliderImages/goaSlider/img6.jpeg";
 // import img7 from "../../Assets/SliderImages/goaSlider/img7.jpeg";
-
-// import "../../css/slide.css";
 
 const Goa = () => {
   return (
