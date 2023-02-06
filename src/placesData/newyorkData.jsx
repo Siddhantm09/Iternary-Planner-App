1f8874e01e09662e54915843846f67a2c5c1e3f6
Siddhantm09 <mahatosiddhant29@gmail.com>
2023-02-06 13:50:55 +0530

    folder struc

--- src/placesData/newyorkData.jsx
+++ src/placesData/newyorkData.jsx
@@ -1,7 +1,7 @@
-import ny1 from "../Assets/SliderImages/NewYorkSlider/img1.png";
-import ny2 from "../Assets/SliderImages/NewYorkSlider/img2.png";
-import ny3 from "../Assets/SliderImages/NewYorkSlider/img3.png";
-import ny4 from "../Assets/SliderImages/NewYorkSlider/img4.png";
+import ny1 from "../assets/sliderImages/NewYorkSlider/img1.png";
+import ny2 from "../assets/sliderImages/NewYorkSlider/img2.png";
+import ny3 from "../assets/sliderImages/NewYorkSlider/img3.png";
+import ny4 from "../assets/sliderImages/NewYorkSlider/img4.png";
 
 export const NewyorkImages = [ny1, ny2, ny3, ny4];
 export const HotelsData = [
